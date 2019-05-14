0812ae9ada31c6b745520dccf59e1b09ba9cd275
tyler stuart <t.stu21.11@gmail.com>
2019-05-13 18:45:25 -0700

    added logic and server code

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,44 @@
 //JS HERE
+
+var friendsData = [
+    {
+      name: "Michael Corleone",
+      photo: "https://via.placeholder.com/350x150",
+      scores: [3, 4, 3, 3, 4, 4, 4, 1, 5, 3]
+    },
+    {
+      name: "Barack Obama",
+      photo:"https://via.placeholder.com/350x150",
+      scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    },
+    {
+      name: "Scorpion",
+      photo: "https://via.placeholder.com/350x150",
+      scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+      name: "Mario",
+      photo:"https://via.placeholder.com/350x150",
+      scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+    {
+      name: "Pikachu",
+      photo:"https://via.placeholder.com/350x150",
+      scores: [5, 4, 4, 4, 4, 3, 2, 1, 5, 5]
+    },
+    {
+      name: "Arnolds Fist",
+      photo:"https://via.placeholder.com/350x150",
+      scores: [2, 4, 2, 4, 2, 3, 4, 4, 4, 3]
+    },
+    {
+      name: "Spongebob",
+      photo:"https://via.placeholder.com/350x150",
+      scores: [5, 4, 5, 4, 5, 3, 3, 3, 2, 4]
+    }
+  ];
+  
+  
+  
+  // Export to API Route
+  module.exports = friendsData;
---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var apiRoutes = require('../Friend-finder/app/routing/api-routes');
+var htmlRoutes = require('../Friend-finder/app/routing/html-routes');
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use('/static', express.static(path.join(__dirname + 'app/public/css')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+apiRoutes(app); 
+htmlRoutes(app); 
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
